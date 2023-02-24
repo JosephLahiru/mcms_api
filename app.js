57ea1c8a1dc31b9a5f5c1711a4e9bf9f6342428a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-24 09:56:40 +0000

    added setdoctor post method

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
+// const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
+
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
 });
 
-app.get('/doctors', (req, res) => {
+app.get('/get_doctors', (req, res) => {
     const sql = 'SELECT * FROM doctor';
     db.query(sql, (err, result) => {
         if (err) {
@@ -18,6 +22,19 @@ app.get('/doctors', (req, res) => {
         res.json(result);
     });
 });
+
+// app.post('/set_doctors', (req, res) => {
+//     const { d_id, first_name, last_name, nic, email, address, contact_no } = req.body;
+//     const sql = 'INSERT INTO doctor (d_id, first_name, last_name, nic, email, address, contact_no) VALUES (?, ?, ?, ?, ?, ?, ?)';
+//     db.query(sql, [d_id, first_name, last_name, nic, email, address, contact_no], (err, result) => {
+//         if (err) {
+//             console.error('Error executing query: ', err);
+//             res.status(500).json({ error: 'Internal server error.' });
+//             return;
+//         }
+//         res.json({ message: 'Doctor added successfully.' });
+//     });
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
