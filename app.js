afb898b0a76c2672afe955512284d0f29b74b9b8
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-25 07:19:36 +0000

    added post requst for adding a doctor

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const db = require('./db');
 
 const app = express();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, world!');
@@ -23,18 +23,18 @@ app.get('/get_doctors', (req, res) => {
     });
 });
 
-// app.post('/set_doctors', (req, res) => {
-//     const { d_id, first_name, last_name, nic, email, address, contact_no } = req.body;
-//     const sql = 'INSERT INTO doctor (d_id, first_name, last_name, nic, email, address, contact_no) VALUES (?, ?, ?, ?, ?, ?, ?)';
-//     db.query(sql, [d_id, first_name, last_name, nic, email, address, contact_no], (err, result) => {
-//         if (err) {
-//             console.error('Error executing query: ', err);
-//             res.status(500).json({ error: 'Internal server error.' });
-//             return;
-//         }
-//         res.json({ message: 'Doctor added successfully.' });
-//     });
-// });
+app.post('/set_doctors', (req, res) => {
+    const { d_id, first_name, last_name, nic, email, address, contact_no } = req.body;
+    const sql = 'INSERT INTO doctor (d_id, first_name, last_name, nic, email, address, contact_no) VALUES (?, ?, ?, ?, ?, ?, ?)';
+    db.query(sql, [d_id, first_name, last_name, nic, email, address, contact_no], (err, result) => {
+        if (err) {
+            console.error('Error executing query: ', err);
+            res.status(500).json({ error: 'Internal server error.' });
+            return;
+        }
+        res.json({ message: 'Doctor added successfully.' });
+    });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
