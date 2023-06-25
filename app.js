c9d90ab05c9118cd11b361df5b1c47dd89f561a3
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-06-25 15:47:01 +0000

    created Update Stock By Product ID endpoint

--- app.js
+++ app.js
@@ -56,7 +56,8 @@ const endpoints = {
     "Get Stock Types": '/get_stock_types',
     "Get Expire Types": '/get_expire_types',
     "Get Returning And None": "/get_returning_none",
-    "Set Billing": "/set_billing"
+    "Set Billing": "/set_billing",
+    "Update Stock By Product ID": "/update_stock/:prdct_id"
 }
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -760,6 +761,21 @@ app.get(endpoints["Get Expire Types"], (req, res) => {
             return;
         }
         res.json(result);
+    });
+});
+
+// Update Stock By Product ID
+app.put(endpoints["Update Stock By Product ID"], (req, res) => {
+    const { brand_name, prdct_name, mfd_date, exp_date, ac_price, sell_price, med_type, stock_type, expire_type } = req.body;
+    const _prdct_id = req.params.prdct_id;
+    const sql = 'UPDATE stock SET brand_name=?, prdct_name=?, mfd_date=?, exp_date=?, ac_price=?, sell_price=?, med_type=?, stock_type=?, expire_type=? WHERE prdct_id=?';
+    db.query(sql, [brand_name, prdct_name, mfd_date, exp_date, ac_price, sell_price, med_type, stock_type, expire_type, _prdct_id], (err, result) => {
+        if (err) {
+            console.error('Error executing query: ', err);
+            res.status(500).json({ error: 'Internal server error.' + err });
+            return;
+        }
+        res.json({ message: 'Stock updated successfully.' });
     });
 });
 
